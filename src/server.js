1ef9743baf4aade1ad090fee6672c976a6846604
tyudihw <tyudihw@gmail.com>
2021-09-27 17:20:10 +0700

    user registration implemented

--- src/server.js
+++ src/server.js
@@ -1,17 +1,22 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-// const routes = require('./routes');
+
+// notes
 const notes = require('./api/notes');
-// const NotesService = require('./services/inMemory/NotesService'); #ganti ke postgres
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
-    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -21,13 +26,22 @@ const init = async () => {
 
   // server.route(routes);
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
